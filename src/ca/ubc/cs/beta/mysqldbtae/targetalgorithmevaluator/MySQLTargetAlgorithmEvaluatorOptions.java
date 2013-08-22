b0851d771ae3ab1a3e1e8aa36cde9faaf0edcc4c
Steve Ramage <seramage@cs.ubc.ca>
2013-08-22 15:52:57 -0700

    Changed Option Levels and Changed MYSQL Connector Version for no real reason other than to keep it up to date.

--- src/ca/ubc/cs/beta/mysqldbtae/targetalgorithmevaluator/MySQLTargetAlgorithmEvaluatorOptions.java
+++ src/ca/ubc/cs/beta/mysqldbtae/targetalgorithmevaluator/MySQLTargetAlgorithmEvaluatorOptions.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 
 import ca.ubc.cs.beta.aclib.misc.file.HomeFileUtils;
 import ca.ubc.cs.beta.aclib.misc.jcommander.validator.FixedPositiveInteger;
+import ca.ubc.cs.beta.aclib.misc.options.OptionLevel;
 import ca.ubc.cs.beta.aclib.misc.options.UsageTextField;
 import ca.ubc.cs.beta.aclib.options.AbstractOptions;
 import ca.ubc.cs.beta.mysqldbtae.JobPriority;
@@ -13,7 +14,7 @@ import ca.ubc.cs.beta.mysqldbtae.JobPriority;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterFile;
 
-@UsageTextField(title="MySQL Target Algorithm Evaluator Options", description="Options that control the MySQL Target Algorithm Evaluator")
+@UsageTextField(title="MySQL Target Algorithm Evaluator Options", description="Options that control the MySQL Target Algorithm Evaluator", level=OptionLevel.INTERMEDIATE)
 public class MySQLTargetAlgorithmEvaluatorOptions extends AbstractOptions{
 
 	@UsageTextField(defaultValues="~/.aclib/mysqldbtae.opt")
@@ -21,13 +22,13 @@ public class MySQLTargetAlgorithmEvaluatorOptions extends AbstractOptions{
 	@ParameterFile(ignoreFileNotExists = true) 
 	public File smacDefaults = HomeFileUtils.getHomeFile(".aclib" + File.separator  + "mysqldbtae.opt");
 	
-	
 	@Parameter(names="--mysqldbtae-pool", description="Pool to take tasks from")
 	public String pool;
 	
 	@Parameter(names="--mysqldbtae-create-pool", description="Attempt to create the tables for the database")
 	public boolean createTables = true;
 	
+	@UsageTextField(level=OptionLevel.ADVANCED)
 	@Parameter(names="--mysqldbtae-batch-insert-size", description="The number of runs to batch into a single insert", validateWith=FixedPositiveInteger.class)
 	public int batchInsertSize = 500;
 	
@@ -37,6 +38,7 @@ public class MySQLTargetAlgorithmEvaluatorOptions extends AbstractOptions{
 	@Parameter(names="--mysqldbtae-delete-partition-on-shutdown", description="If set to true, we will delete all data in the run partition (this requires that the partition be set to something greater than zero)")
 	public boolean deletePartitionDataOnShutdown;
 	
+	@UsageTextField(level=OptionLevel.DEVELOPER)
 	@Parameter(names="--mysqldbtae-path-strip", description="If a trimmed path to be written to the database starts with the trimmed version of this string, we will remove this string from the start. This is useful if you mount the root of some remote file system that the workers will execute on in some directory. You should not put a / at the end of this string")
 	public String pathStrip;
 	
@@ -66,16 +68,20 @@ public class MySQLTargetAlgorithmEvaluatorOptions extends AbstractOptions{
 	public JobPriority priority = JobPriority.NORMAL;
 	
 	//This is almost certain premature optimization
+	@UsageTextField(level=OptionLevel.DEVELOPER)
 	@Parameter(names={"--mysqldbtae-get-addl-run-data", "--mysqldbtae-get-additional-run-data", "--mysqldbtae-get-full-run"}, description="Get the Additional Run Data field from the database")
 	public boolean additionalRunData = true;
 
+	@UsageTextField(level=OptionLevel.ADVANCED)
 	@Parameter(names={"--mysqldbtae-wake-up-on-submit"}, description="Wake the workers up when new jobs are submitted")
 	public boolean wakeUpWorkersOnSubmit;
 
-	@UsageTextField(defaultValues="Twice the number of available processors")
+	@UsageTextField(defaultValues="Twice the number of available processors", level=OptionLevel.DEVELOPER)
 	@Parameter(names={"--mysqldbtae-poll-threads"}, description="How many threads will be used to manage the outstanding requests")
 	public int pollPoolSize = Runtime.getRuntime().availableProcessors() * 2;
 
+
+	@UsageTextField(level=OptionLevel.DEVELOPER)
 	@Parameter(names="--mysqldbtae-poll-delay", description="How often (in milliseconds) to poll the database for new results")
 	public long delayBetweenPolls = 2000;
 
